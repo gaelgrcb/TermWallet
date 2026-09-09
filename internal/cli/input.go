b43e39961f96bef6b9ef30c38e821e14889ff8b3
gaelgrcb <gaelgarciabst@gmail.com>
2026-09-09 08:02:53 -0700

    feat: add input helper for numeric console options

--- internal/cli/input.go
+++ internal/cli/input.go
@@ -1,1 +1,19 @@
 package cli
+
+import "fmt"
+
+func Input() int {
+	for {
+		var op int
+		_, err := fmt.Scanln(&op)
+
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a valid number.")
+			var clean string
+			fmt.Scanln(&clean)
+			continue
+		}
+
+		return op
+	}
+}
